73fc2f485ff4d4b95cc53bd3637ac9b9102231bd
Adrian <adrian_michot@yahoo.com>
2019-03-28 13:15:37 -0500

    implements delete note with context against json server

--- src/main/NoteMain.js
+++ src/main/NoteMain.js
@@ -1,28 +1,44 @@
 import React from "react";
 //components
 import { getDate } from "../notes-helpers";
+//helperfunctions
+import { findNote } from "../notes-helpers";
+//context
+import NotefulsContext from "../NotefulsContext";
 //css
 import "./NoteMain.css";
 
-export default function NoteMain(props) {
-	const note = props.note[0];
-	return (
-		<section>
-			<ul className='noteMain' aria-live='polite'>
-				{note && (
-					<li className='NoteItem' key={note.id}>
-						<h3 className='NoteItem__title'>{note.name}</h3>
-						<p className='NoteItem__date'>
-							Date modified on <time>{getDate(note.modified)}</time>
-						</p>
-						<p>
-							{" "}
-							Content: <br />
-							{note.content}
-						</p>
-					</li>
-				)}
-			</ul>
-		</section>
-	);
+export default class NoteMain extends React.Component {
+	static defaultProps = {
+		match: {
+			params: {}
+		}
+	};
+	static contextType = NotefulsContext;
+
+	render() {
+		const { notes = [] } = this.context;
+		const { noteId } = this.props.match.params;
+		const note = findNote(notes, noteId) || { content: "" };
+		console.log(note);
+		return (
+			<section>
+				<ul className='noteMain' aria-live='polite'>
+					{note && (
+						<li className='NoteItem' key={note.id}>
+							<h3 className='NoteItem__title'>{note.name}</h3>
+							<p className='NoteItem__date'>
+								Date modified on <time>{getDate(note.modified)}</time>
+							</p>
+							<p>
+								{" "}
+								Content: <br />
+								{note.content}
+							</p>
+						</li>
+					)}
+				</ul>
+			</section>
+		);
+	}
 }
