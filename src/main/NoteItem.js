753da70a3ae6208a1969069c6195c4f24fa7dc57
Adrian <adrian_michot@yahoo.com>
2019-03-28 13:13:24 -0500

    implements delete note with context against json server

--- src/main/NoteItem.js
+++ src/main/NoteItem.js
@@ -1,14 +1,53 @@
+//dependencies
 import React from "react";
+//context
+import NotefulsContext from "../NotefulsContext";
+//css
 import "./NoteItem.css";
+//helper functions
 import { getDate } from "../notes-helpers";
 
-export default function NoteItem(props) {
-	return (
-		<li className='NoteItem' key={props.id}>
-			<h3 className='NoteItem__title'>{props.name}</h3>
-			<p className='.Note__dates'>
-				Date modified on<time> {getDate(props.modified)}</time>
-			</p>
-		</li>
-	);
+export default class NoteItem extends React.Component {
+	static contextType = NotefulsContext;
+	static defaultProps = {
+		onDeleteNote: () => {}
+	};
+
+	deleteNoteRequest = e => {
+		e.preventDefault();
+		const noteId = this.props.id;
+		fetch(`http://localhost:9090/notes/${noteId}`, {
+			method: "DELETE",
+			headers: { "content-type": "application/json" }
+		})
+			.then(res => {
+				if (!res.ok) return res.json().then(e => Promise.reject(e));
+				return res.json();
+			})
+			.then(() => {
+				this.context.deleteNote(noteId);
+				// allow parent to perform extra behaviour
+				this.props.onDeleteNote(noteId);
+			})
+			.catch(error => {
+				console.error({ error });
+			});
+	};
+	render() {
+		const { name, id, modified } = this.props;
+		return (
+			<li className='NoteItem' key={id}>
+				<h3 className='NoteItem__title'>{name}</h3>
+				<button
+					className='Note__delete'
+					type='button'
+					onClick={this.deleteNoteRequest}>
+					remove
+				</button>
+				<p className='.Note__dates'>
+					Date modified on<time> {getDate(modified)}</time>
+				</p>
+			</li>
+		);
+	}
 }
